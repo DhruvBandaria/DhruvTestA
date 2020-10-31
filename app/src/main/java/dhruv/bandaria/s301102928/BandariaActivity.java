9282aeadeb23a624c5e5ba7c380f8c1f706ef58f
Dhruv <dhruvbandaria@hotmail.com>
2020-10-31 08:52:11 -0400

    Initial commit

---
+++ app/src/main/java/dhruv/bandaria/s301102928/BandariaActivity.java
@@ -0,0 +1,19 @@
+package dhruv.bandaria.s301102928;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+Name: Dhruv Bandaria
+Section:02
+Student Number: 301102928
+
+ */
+public class BandariaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bandaria);
+    }
+}
